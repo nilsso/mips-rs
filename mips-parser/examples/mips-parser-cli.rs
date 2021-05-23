e7a5fb63f989aa537a20685a3ce2fd7d5417d05d
Nils Olsson <nilso@enosis.net>
2021-05-22 23:17:47 -0700

    Lazy update

--- mips-parser/examples/mips-parser-cli.rs
+++ mips-parser/examples/mips-parser-cli.rs
@@ -4,79 +4,85 @@
 #![allow(dead_code)]
 #![allow(unused_parens)]
 
-use clap::{load_yaml, App, ArgMatches};
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
+use clap::{load_yaml, App, ArgMatches};
 use pest::{error::Error as PestError, iterators::Pairs};
 
 use mips_parser::prelude::*;
-
-fn cli() -> Result<(), MipsParserError> {
-    let yaml = load_yaml!("mips-parser-cli.yaml");
-    let matches = App::from_yaml(yaml).get_matches();
-
-    let output = matches.value_of("output").unwrap();
-
-    // println!("{:?}", matches);
-
-    let as_peg = (output == "peg");
-    let as_ast = (output == "ast");
-
-    let stdin = std::io::stdin();
-    let mut buffer = String::new();
-    while stdin.read_line(&mut buffer).map(|b| b > 0).unwrap_or(false) {
-        let input = buffer.trim_end().to_owned();
-        buffer.clear();
-
-        let pairs = MipsParser::parse(Rule::expr, &input);
-        if let Err(e) = &pairs {
-            println!("{:?}", e);
-            continue;
-        }
-        let pair = pairs
-            .unwrap()
-            .first_inner()
-            .or(Err(MipsParserError::AstError(AstError::InsufficientPairs)))?;
-        if as_peg {
-            println!("{:?}", pair);
-            continue;
-        }
-        let ast_res = Expr::from_pair(pair).map_err(MipsParserError::AstError);
-        if let Err(e) = &ast_res {
-            println!("{:?}", e);
-            continue;
-        }
-        let ast = ast_res.unwrap();
-        if as_ast {
-            println!("{:?}", ast);
-            continue;
-        }
-        println!("{}", ast);
-    }
-
-    // let mut pairs: Vec<Result<Pairs<Rule>, PestError<Rule>>> = Vec::new();
-
-    Ok(())
-}
 
 fn main() {
     if let Err(err) = cli() {
         println!("Error: {:?}", err);
     }
-    // use std::fs::read_to_string;
-    // let path = "./example-scripts/solar.mips";
-    // let source = read_to_string(path).unwrap();
-    // let source = "s drr2 Parameter rr3";
-    // println!("{}", source);
-    // let pairs = MipsParser::parse(Rule::program, &source);
-    // let pair = pairs.unwrap().next().unwrap();
-    // let ast = Program::from_pair(program_pair);
-    // let pair = MipsParser::parse(Rule::func, &source).unwrap().inner();
-    // let ast = Expr::from_pair(pair);
-    // println!("------------------");
-    // for expr in ast.iter() {
-    //     println!("{:?}", expr);
-    // }
-    // println!("{:?}", ast);
-    // println!("------------------");
-    // println!("{}", ast);
+}
+
+fn cli() -> Result<(), std::io::Error> {
+    let yaml = load_yaml!("mips-parser-cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let pretty = (matches.occurrences_of("pretty") > 0);
+    let output = matches.value_of("output").unwrap();
+    let file = matches.value_of("file");
+    // let pretty = matches.value_of
+
+    let as_peg = (output == "peg");
+    let as_ast = (output == "ast");
+
+    if let Some(path) = file {
+        let file = std::fs::File::open(path)?;
+        for line in std::io::BufReader::new(file).lines() {
+            exec_line(&line?, as_peg, as_ast, pretty);
+        }
+    } else {
+        let stdin = std::io::stdin();
+        let mut buffer = String::new();
+        while stdin.read_line(&mut buffer).map(|b| b > 0).unwrap_or(false) {
+            let line = buffer.to_owned();
+            buffer.clear();
+            exec_line(&line, as_peg, as_ast, pretty);
+        }
+    }
+    Ok(())
+}
+
+fn exec_line(line: &String, as_peg: bool, as_ast: bool, pretty: bool) {
+    let pairs = MipsParser::parse(Rule::expr, &line);
+    if let Err(e) = &pairs {
+        println!("{:?}", e);
+        return;
+    }
+    let pair_res = pairs
+        .unwrap()
+        .first_inner()
+        .or(Err(MipsParserError::AstError(AstError::InsufficientPairs)));
+    if let Err(e) = &pair_res {
+        println!("{:?}", e);
+        return;
+    }
+    let pair = pair_res.unwrap();
+    if as_peg {
+        if pretty {
+            println!("{:#?}", pair);
+        } else {
+            println!("{:?}", pair);
+        }
+        return;
+    }
+    let ast_res = Expr::from_pair(pair).map_err(MipsParserError::AstError);
+    if let Err(e) = &ast_res {
+        println!("{:?}", e);
+        return;
+    }
+    let ast = ast_res.unwrap();
+    if as_ast {
+        if pretty {
+            println!("{:#?}", ast);
+        } else {
+            println!("{:?}", ast);
+        }
+        return;
+    }
+    println!("{}", ast);
 }
